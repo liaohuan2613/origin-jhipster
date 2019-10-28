e7a94f5fd2a0d3f201fdc474f4618a66b60271d4
liaohuan2613 <liaohuan7876@dingtalk.com>
2019-10-28 20:26:24 +0800

    clear other file

--- src/main/java/tech/deepq/origin/config/DatabaseConfiguration.java
+++ src/main/java/tech/deepq/origin/config/DatabaseConfiguration.java
@@ -1,7 +1,5 @@
 package tech.deepq.origin.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import io.github.jhipster.config.JHipsterConstants;
 import io.github.jhipster.config.liquibase.AsyncSpringLiquibase;
 import liquibase.integration.spring.SpringLiquibase;
@@ -9,15 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -54,50 +49,6 @@ public class DatabaseConfiguration {
             log.debug("Configuring Liquibase");
         }
         return liquibase;
-    }
-
-    @Primary
-    @Bean(name = "hikariConfig")
-    @Qualifier(value = "hikariConfig")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public HikariConfig hikariConfig() {
-        return new HikariConfig();
-    }
-
-    @Primary
-    @Bean(name = "dataSource")
-    @Qualifier(value = "dataSource")
-    public DataSource dataSource(@Qualifier("hikariConfig") HikariConfig hikariConfig) {
-        return new HikariDataSource(hikariConfig);
-    }
-
-    @Primary
-    @Bean("jdbcTemplate")
-    public JdbcTemplate jdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
-
-    @Bean("secondaryJdbcTemplate")
-    public JdbcTemplate secondaryJdbcTemplate(@Qualifier("secondaryDataSource") DataSource secondaryDataSource) {
-        return new JdbcTemplate(secondaryDataSource);
-    }
-
-    @Bean(name = "secondaryHikariConfig")
-    @Qualifier(value = "secondaryHikariConfig")
-    @ConfigurationProperties(prefix = "secondary.datasource")
-    public HikariConfig secondaryHikariConfig() {
-        return new HikariConfig();
-    }
-
-
-    @Bean(name = "secondaryDataSource")
-    @Qualifier(value = "secondaryDataSource")
-    public DataSource secondaryDataSource(DataSource dataSource, @Qualifier("secondaryHikariConfig") HikariConfig secondaryHikariConfig) {
-        if (secondaryHikariConfig.getJdbcUrl() == null || "".equals(secondaryHikariConfig.getJdbcUrl())) {
-            log.info("SecondaryJdbcUrl is NULL!");
-            return dataSource;
-        }
-        return new HikariDataSource(secondaryHikariConfig);
     }
 
 }
